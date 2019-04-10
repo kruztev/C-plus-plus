44a93e3cc2db4810be6c316b38b2917d43f711fb
Alexander Kruztev <juniork@abv.bg>
2019-04-10 22:32:32 +0300

    Regular update

--- OOP Practices/OOP Practices/main.cpp
+++ OOP Practices/OOP Practices/main.cpp
@@ -5,190 +5,89 @@
 
 #include <iostream>
 #include <fstream>
-#include <math.h>
-#include "Clock.h"
 
-const double DIFFERENCE = 0.0000001;
+// Problem solving test - work with files.
 
-struct Point
+const unsigned short MAX_NAME_LENGTH = 256;
+
+struct Student
 {
-    double x, y;
-    
-    void readInData();
-    void print() const;
-    double distanceToCenter() const;
-    double distanceBetweenTwoPoints(const Point&) const;
+    char firstName[MAX_NAME_LENGTH];
+    char lastName[MAX_NAME_LENGTH];
+    unsigned long long fn;
+    unsigned short grade;
 };
 
-size_t closestPoint(const Point* arr, const unsigned int size)
+void create()
 {
-    size_t indexToReturn = 0;
-    for (size_t index = 1; index < size - 1; ++index)
-    {
-        if (arr[index].x > 0 && arr[index].x < arr[indexToReturn].x &&
-            arr[index].y > 0 && arr[index].y < arr[indexToReturn].y)
-       {
-           indexToReturn = index;
-       }
-    }
-    return indexToReturn;
-}
-
-void readArrayFromStandartInput(Point* array, const unsigned int size)
-{
-    for (size_t i = 0; i < size - 1; i++)
-    {
-        std::cin >> array[i].x;
-        std::cin >> array[i].y;
-    }
-    return;
-}
-
-void readArrayFromFile(Point* array, std::ifstream& fileIn, size_t size)
-{
-    for (size_t i = 0; i < size; ++i)
-    {
-        if (!fileIn)
-        {
-            break;
-        }
-        fileIn >> array[i].x;
-        fileIn >> array[i].y;
-    }
-    return;
-}
-
-void Point::readInData()
-{
-    std::cin >> x >> y;
-    return;
-}
-
-void Point::print() const
-{
-    std:: cout << "x = " << x << ' ' << "y = " << y << std::endl;
-    return;
-}
-
-double Point::distanceToCenter() const
-{
-    double distance = sqrt(x*x + y*y);
-    return distance;
+    Student temp;
+    std::cin >> temp.fn;
+    std::cin >> temp.firstName;
+    std::cin >> temp.lastName;
+    std::cin >> temp.grade;
     
-}
-
-double Point::distanceBetweenTwoPoints(const Point& secondPoint) const
-{
-    double differenceOfX = (x-secondPoint.x)*(x-secondPoint.x);
-    double differenceOfY = (y-secondPoint.y)*(y-secondPoint.y);
-    double distance = sqrt(differenceOfX + differenceOfY);
-    return distance;
-}
-
-struct Rectangle
-{
-    Point startPoint;
-    Point pointAtEnd;
-    
-    void readInRectangle();
-    void printRectangle() const;
-};
-
-bool compareDouble(double a, double b)
-{
-    if (std::max(a,b) - std::min(a,b) < DIFFERENCE)
+    std::ofstream file("StudentsGrades.db", std::ios::binary | std::ios::app);
+    if(!file)
     {
-        return 1;
-    }
-    return 0;
-}
-
-void Rectangle::readInRectangle()
-{
-    startPoint.readInData();
-    pointAtEnd.readInData();
-    if(compareDouble(startPoint.x, pointAtEnd.x) && compareDouble(startPoint.y, pointAtEnd.y))
-    {
-        std::cout << "There is no such rectangle\n";
+        std::cerr << "Cannot open file\n";
         return;
     }
-    return;
+    file.write((const char*)&temp, sizeof(Student));
+    std::cout << "Record saved!\n";
+    file.close();
 }
 
-void Rectangle::printRectangle() const
+void sequentialSearch()
 {
-    double height = std::max(startPoint.y, pointAtEnd.y) - std::min(startPoint.y, pointAtEnd.y);
-    double width = std::max(startPoint.x, pointAtEnd.x) - std::min(startPoint.x, pointAtEnd.x);
-    for (size_t i = 0; i < (size_t) height; ++i) // Печата редовете
+    Student temp;
+    unsigned long long fn;
+    std::cin >> fn;
+    std::fstream file("StudentsGrades.db", std::ios::binary);
+    if(!file)
     {
-        if (i == 0)
+        std::cerr << "Cannot open file\n";
+        return;
+    }
+    
+    while(!file.eof())
+    {
+        file.read((char*)&temp, sizeof(Student));
+        if (temp.fn == fn)
         {
-            for(size_t j = 0; j < (size_t )width; ++j)
-            {
-                std::cout << '*';
-            }
-            std::cout << std::endl;
-        }
-        if(i == height - 1)
-        {
-            
-            for(size_t j = 0; j < width; j++)
-            {
-                std::cout << '*';
-            }
-            std::cout << std::endl;
+            std::cout << temp.fn << ' ' << temp.firstName << ' ' << temp.lastName << ' ' << temp.grade;
             break;
         }
-        
-        size_t temp = 1; //  Временна променлива, която контролира печатането на самия ред
-        std::cout << '*';
-        while(temp != width - 1)
+        if (file.eof())
         {
-            std::cout << ' ';
-            temp++;
+            break;
         }
-        std::cout << '*';
-        std::cout << std::endl;
+        std::cout << "Record not found!\n";
     }
+    file.close();
 }
 
 int main()
 {
+    const char* dictionary[] = {"create", "sequentialSearch", "exit"};
+    char buffer[1024];
+    while(true)
+    {
+        std::cin >> buffer;
+        if (!strcmp(buffer, dictionary[0]))
+        {
+            create();
+            continue;
+        }
+        if(!strcmp(buffer, dictionary[1]))
+        {
+            sequentialSearch();
+            continue;
+        }
+        if(!strcmp(buffer, dictionary[2]))
+        {
+            break;
+        }
+    }
     
-    
-    
-    Clock a;
-    a.syncTime();
-    a.print();
     return 0;
-    
-//    Point* arr = nullptr;
-//    std::ifstream fileIn;
-//    fileIn.open("points.txt", std::ios::in);
-//    if (!fileIn)
-//    {
-//        std::cerr << "Cannot open file\n";
-//        return -1;
-//    }
-//    size_t size = 0;
-//    fileIn >> size;
-//    arr = new (std::nothrow) Point[size];
-//    if(!arr)
-//    {
-//        std::cerr << "Cannot allocate memory for points\n";
-//        return -1;
-//    }
-//    readArrayFromFile(arr, fileIn, size);
-//    std::cout << "Count of points: " << size << '\n';
-//    for (size_t i = 0; i < size; ++i)
-//    {
-//        std::cout << arr[i].x << ' ' << arr[i].y << '\n';
-//    }
-//
-//    return 0;
-
-    //    Rectangle x;
-    //    x.readInRectangle();
-    //    x.printRectangle();
-
 }
